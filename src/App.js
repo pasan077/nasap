8fd59a5d76731ab38d24d24991256696523369e9
Pasan Jayathilaka <pasan4all@gmail.com>
2021-04-02 11:28:09 +0530

    02-04-2021-1128hrs

--- src/App.js
+++ src/App.js
@@ -4,20 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>nasap app</h1>
+      <h3>Application development</h3>
+      <br/>
+      <p>Login</p>
+      <text></text>
+      <p>Password</p>
+      <password></password>
+      <button>Login</button>
+      <button>Reset</button>
     </div>
   );
 }
